127c00760b3739a2357a0b70f9ffd49abcb41dfe
cesarbrancalhao <58019405+cesarbrancalhao@users.noreply.github.com>
2024-02-12 14:15:12 -0300

    Added hero and explore button

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,26 @@
+"use client";
+
 import Image from 'next/image'
+import CustomButton from './CustomButton'
 
 const Hero = () => {
+    const handleScroll = () => {
+        
+    }
+
   return (
-    <div>Hero</div>
+    <div className='hero'>
+        <div className='flex-1 pt-36 padding-x'>
+            <h1 className='hero__title'>Find the perfect car for you here!</h1>
+
+            <p className='hero__subtitle'>The best rental for your next trip.</p>
+
+            <CustomButton
+            title="Explore Cars"
+            containerStyles="bg-primary-blue text-white rounded-full mt-10"
+            handleClick={handleScroll} />
+        </div>
+    </div>
   )
 }
 
