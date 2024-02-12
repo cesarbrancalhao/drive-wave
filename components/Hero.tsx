465442ba69afc8a1a9019df473c1f1e8347059e1
cesarbrancalhao <58019405+cesarbrancalhao@users.noreply.github.com>
2024-02-12 14:28:19 -0300

    Added assets

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,6 +10,7 @@ const Hero = () => {
 
   return (
     <div className='hero'>
+        
         <div className='flex-1 pt-36 padding-x'>
             <h1 className='hero__title'>Find the perfect car for you here!</h1>
 
@@ -19,6 +20,15 @@ const Hero = () => {
             title="Explore Cars"
             containerStyles="bg-primary-blue text-white rounded-full mt-10"
             handleClick={handleScroll} />
+        </div>
+
+        <div className='hero__image-container'>
+
+        </div>
+        <div className='hero__image'>
+            <Image src='/hero.png' alt='' fill className='object-contain' />
+
+            <div className='hero__image-overlay' />
         </div>
     </div>
   )
