6e09739bbc42a534b435636c1ec40360afcd426a
cesarbrancalhao <58019405+cesarbrancalhao@users.noreply.github.com>
2024-02-12 14:42:14 -0300

    Added Navbar and Footer

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import Image from 'next/image'
+
+const Footer = () => {
+    
+}
+
+export default Footer
---
+++ components/Navbar.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import Image from 'next/image'
+
+const Navbar = () => {
+    
+}
+
+export default Navbar
