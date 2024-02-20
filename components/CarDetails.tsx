40cda320272c136075420ec4bae94360bece3381
cesarbrancalhao <58019405+cesarbrancalhao@users.noreply.github.com>
2024-02-20 16:17:28 -0300

    Added modal info and images, fixed stretching images

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -38,10 +38,10 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                             leaveFrom="opacity-100 scale-100"
                             leaveTo="opacity-0 scale-85">
 
-                            <Dialog.Panel 
+                            <Dialog.Panel
                             className="relative w-full max-w-lg max-h-[90vh]
-                            overflow-y-auto transform rounded-2xl 
-                            bg-white text-left shadow-xsl transition-all
+                            overflow-y-auto transform rounded-2xl p-6
+                            bg-white text-left shadow-xl transition-all
                             flex flex-col gap-5">
 
                                 <button
@@ -60,23 +60,43 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                                 <div className="flex-1 flex flex-col gap-3">
 
                                     <div className="relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg">
-                                        <Image src="/hero.png" alt="Car model" fill priority />
+                                        <Image src="/hero.png" alt="Car model" fill priority className="object-contain" />
                                     </div>
 
                                     <div className="flex gap-3">
 
                                         <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
-                                            <Image src="/hero.png" alt="Car model" fill priority />
+                                            <Image src="/hero.png" alt="Car model" fill priority className="object-contain" />
                                         </div>
                                         <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
-                                            <Image src="/hero.png" alt="Car model" fill priority />
+                                            <Image src="/hero.png" alt="Car model" fill priority className="object-contain" />
                                         </div>
                                         <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
-                                            <Image src="/hero.png" alt="Car model" fill priority />
+                                            <Image src="/hero.png" alt="Car model" fill priority className="object-contain" />
                                         </div>
 
                                     </div>
                                     
+                                </div>
+
+                                <div className="flex-1 flex flex-col gap-2">
+                                    <h2 className="font-semibold text-xl capitalize">
+                                        {car.make} {car.model}
+                                    </h2>
+                                </div>
+
+                                <div className="mt-3 flex flex-wrap gap-4">
+                                    {Object.entries(car).map(([key, value]) => (
+                                        <div key={key} className="flex justify-between gap-5 w-full text-right">
+                                            <h4 className="text-grey capitalize">
+                                                {key.split("_").join(" ")}
+                                            </h4>
+
+                                            <p className="text-black-100 font-semibold">
+                                                {value}
+                                            </p>
+                                        </div>
+                                    ))}
                                 </div>
 
                             </Dialog.Panel>
