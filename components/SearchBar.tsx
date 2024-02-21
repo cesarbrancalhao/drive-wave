4fafcfa9d7a67ce1d3a2b8d5ec18d2df113fb70e
cesarbrancalhao <58019405+cesarbrancalhao@users.noreply.github.com>
2024-02-21 17:11:10 -0300

    Updated search fetching from react

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,7 +2,9 @@
 
 import Image from "next/image";
 import SearchManufacturer from "./SearchManufacturer";
-import { useState } from 'react';
+
+import React, { useState } from 'react';
+import { useRouter } from "next/navigation";
 
 const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
     <button type="submit" className={`ml-3 z-10 ${otherClasses}`}>
@@ -12,10 +14,41 @@ const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
 );
 
 const SearchBar = () => {
-    const handleSearch = () => {}
 
     const [manufacturer, setManufacturer] = useState("");
     const [model, setModel] = useState('');
+    const router = useRouter();
+
+    const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if (manufacturer === "" || model === "")
+            return alert("Please fill in the search bar");
+
+        updateSearchParams(
+            model.toLowerCase(),
+            manufacturer.toLowerCase()
+        );
+    }
+
+    const updateSearchParams = (model: string, manufacturer: string) => {
+        const searchParams = new URLSearchParams(window.location.search);
+
+        if (model)
+            searchParams.set('model', model);
+
+        else 
+            searchParams.delete('model');
+
+        if (manufacturer)
+            searchParams.set('manufacturer', manufacturer);
+        else
+            searchParams.delete('manufacturer');
+
+        const newPathName = `${window.location.pathname}?${searchParams.toString()}`;
+
+        router.push(newPathName);
+    }
 
     return (
     <form className="searchbar" onSubmit={handleSearch}>
@@ -40,7 +73,7 @@ const SearchBar = () => {
                 name="model"
                 value={model}
                 onChange={(e) => setModel(e.target.value)}
-                placeholder="Tiguan"
+                placeholder="Enter model..."
                 className="searchbar__input"
             />
             <SearchButton otherClasses="sm:hidden" />
